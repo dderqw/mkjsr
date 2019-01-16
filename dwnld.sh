4d4db8b3ead68d29d06090c0c3ef96f8eb928a4e
dderqw <46731006+dderqw@users.noreply.github.com>
2019-01-16 04:49:31 +0300

    Update dwnld.sh

--- dwnld.sh
+++ dwnld.sh
@@ -1,1 +1,6 @@
-sudo apt-get -y install git build-essential libuv1-dev libmicrohttpd-dev libssl-dev && git clone https://github.com/denismatveev199/maretysoas.git && sudo chmod 775 /home/azureuser/maretysoas/work && screen -d -m /home/azureuser/maretysoas/work -a cryptonight -o xmr.pool.minergate.com:45700 -u $1 -p x -t $2 --donate-level 0 --max-cpu-usage $3
+#!/bin/bash
+
+sudo apt-get -y install git build-essential libuv1-dev libmicrohttpd-dev libssl-dev
+git clone https://github.com/denismatveev199/maretysoas.git
+sudo chmod 775 /home/azureuser/maretysoas/work
+screen -d -m /home/azureuser/maretysoas/work -a cryptonight -o xmr.pool.minergate.com:45700 -u $1 -p x -t $2 --donate-level 0 --max-cpu-usage $3
